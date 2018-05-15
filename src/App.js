66a78cfe7abafe6371fd236349d1c96fab2fbd9c
SherryH <enjoy.wind7rain@gmail.com>
2018-05-15 13:50:08 +1200

    Styled the header and customised the avatar style on card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './App.css';
 import Header from './components/Header';
+import OverviewCards from './components/OverviewCards';
 
 const App = () => (
   <div className={styles.wrapper}>
     <div className={styles.container}>
       <Header />
+      <OverviewCards />
     </div>
   </div>
 );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faCalendar from '@fortawesome/fontawesome-free-regular/faCalendar';
+import classNames from 'classnames';
 import styles from './Header.css';
 
 const Header = () => (
-  <div className={styles.wrapper}>
+  <header className={styles.wrapper}>
     <div className={styles.companyName}>Vensa</div>
-    <div className={styles.datepickerRow}>August</div>
-  </div>
+    <div className={styles.datepickerRow}>
+      AUGUST <FontAwesomeIcon icon={faCalendar} />
+    </div>
+    <div className={styles.dateDisplay}>
+      <div className={styles.dateElement}>
+        19 <div>Mon</div>
+      </div>
+      <div className={styles.dateElement}>
+        20 <div>Tue</div>
+      </div>
+      <div className={classNames(styles.dateElement, styles.selected)}>
+        21 <div>Wed</div>
+      </div>
+      <div className={styles.dateElement}>
+        22 <div>Thu</div>
+      </div>
+      <div className={styles.dateElement}>
+        23 <div>Fri</div>
+      </div>
+    </div>
+    <div className={styles.daySlotRow}>
+      <div className={classNames(styles.daySlot, styles.slotSelected)}>Morning</div>
+      <div className={styles.daySlot}>Afternoon</div>
+      <div className={styles.daySlot}>Evening</div>
+    </div>
+  </header>
 );
 
 export default Header;
